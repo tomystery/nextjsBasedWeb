d93b1c76766455d7d0547a8ec4d29d616e5ab357
tomystery <931892155@qq.com>
2026-09-18 12:25:43 +1200

    ci: configure github actions deploy workflow without secrets

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/postgres-js'; // 注意这里是 postgres-js
-import postgres from 'postgres';
 import * as schema from './schema';
 
-const pool = postgres(process.env.DATABASE_URL!, { max: 1 });
- 
-export const db = drizzle(pool, { schema });
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+// 读取环境变量中的连接串
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql,{schema});
+
