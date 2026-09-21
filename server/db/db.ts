fcd98a2fbeb0803983d5be2e75822eee961f819c
tomystery <931892155@qq.com>
2026-09-22 11:20:37 +1200

    fix: add env connectionString placeholder

--- server/db/db.ts
+++ server/db/db.ts
@@ -4,7 +4,9 @@ import * as schema from './schema';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
+const connectionString = process.env.DATABASE_URL ?? 'postgres://placeholder:placeholder@localhost:5432/placeholder';
+
 // 读取环境变量中的连接串
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(connectionString);
 export const db = drizzle(sql,{schema});
 
